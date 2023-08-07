444ea3ec89629932b0b62a56887c612382a10f56
Kapee87 <kapserker@gmail.com>
2023-08-07 11:52:15 -0300

    change base on vite for localhost

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -5,19 +5,15 @@ import Cities from '../pages/cities/Cities'
 import Login from '../pages/login/Login'
 import PageNotFound from '../pages/notFound/PageNotFound'
 
-
-
-
-
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
-        errorElement: <PageNotFound />,
         children: [
             { path: '/', element: <Home /> },
             { path: '/cities', element: <Cities /> },
-            { path: '/login', element: <Login /> }
+            { path: '/login', element: <Login /> },
+            { path: '*', element: <PageNotFound /> }
         ]
     }
 ])
