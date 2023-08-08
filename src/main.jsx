a453a9a15fbd7879d962721bc8bbdfae1d1553ff
Kapee87 <kapserker@gmail.com>
2023-08-08 09:28:44 -0300

    otra prueba de deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createHashRouter } from 'react-router-dom'
+import Layout from './layouts/Layout.jsx'
+import Home from './pages/index/Home.jsx'
+import Cities from './pages/cities/Cities.jsx'
+import Login from './pages/login/Login.jsx'
+import PageNotFound from './pages/notFound/PageNotFound.jsx'
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/cities', element: <Cities /> },
+      { path: '/login', element: <Login /> },
+      { path: '*', element: <PageNotFound /> }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <RouterProvider router={router} />
+
   </React.StrictMode>,
 )
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  homepage: 'https://kapee87.github.io/myTinerary-nahuelMontaner'
+  homepage: 'https://kapee87.github.io/myTinerary-nahuelMontaner/'
 })
